7cbb1bd822f646853a558e51d65ef86f7e3cca12
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-08-04 11:26:43 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function displayName() {
-    const name = document.getElementById('nameInput').value;
-    const greeting = document.getElementById('greeting');
-    if (name) {
-        greeting.textContent = `Hello, ${name}!`;
-    } else {
-        greeting.textContent = 'Please enter your name.';
-    }
-}
